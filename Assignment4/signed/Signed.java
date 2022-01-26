9ddaa549e6e0a0e72c297ccb20b53461765575bc
Arulkumar99 <97900645+Arulkumar99@users.noreply.github.com>
2022-01-26 12:39:26 +0530

    Add files via upload

---
+++ Assignment4/signed/Signed.java
@@ -0,0 +1,12 @@
+class Signed
+{
+public static void main(String args[])
+{
+System.out.println(10>>2);
+System.out.println(-10>>2);
+System.out.println(10<<2);
+System.out.println(-10<<2);
+System.out.println(10>>>2);
+System.out.println(-20>>>2);
+}
+}
